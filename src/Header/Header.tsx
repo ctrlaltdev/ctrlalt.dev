9936c91470cc70b94370d8e4d697e0f6cdc445dd
ctrlaltdev <yorick@ctrlaltdev.xyz>
2018-08-21 22:00:12 -0700

    grinding up to what I want

--- src/Header/Header.tsx
+++ src/Header/Header.tsx
@@ -1,18 +1,30 @@
 import * as React from 'react'
 import logo from '../img/logo.svg'
 import './Header.css'
+import { HashRouter as Router, Route } from 'react-router-dom'
 
 class Header extends React.Component {
   public render() {
     return (
-      <header className="Header">
-        <h1>
+      <Router>
+        <header className="Header">
           <img src={logo} alt="" />
-          CTRL ALT DEV
-        </h1>
-      </header>
+          <span className="startPath">~</span>
+          <Route component={Title} />
+          <input type="text" className="path" data-enable-grammarly="false" autoCorrect="off" autoCapitalize="off" autoFocus spellCheck={false} />
+        </header>
+      </Router>
     )
   }
+}
+
+const Title = ({location}: any) => {
+  if (location.pathname === "/") {
+    return <h1 className="hidden">CTRL_ALT_DEV</h1>
+  } else {
+    return <h1>{location.pathname}</h1>
+  }
+  
 }
 
 export default Header
--- src/Projects/Project.tsx
+++ src/Projects/Project.tsx
@@ -1,8 +1,10 @@
 import * as React from 'react'
 import './Project.css'
+import { Link } from 'react-router-dom'
 
 interface InterfaceProjectComponent {
   project: {
+    id: string,
     name: string,
     content: [string],
     links: [{
@@ -21,19 +23,9 @@ class Project extends React.Component<InterfaceProjectComponent, {}> {
     return (
       <li className="Project">
         <article>
-          <header className="ProjectName">
+          <Link to={this.props.project.id}>
             <h3>{this.props.project.name}</h3>
-          </header>
-          <div className="ProjectInfo">
-            <div className="ProjectDesc">
-              {this.createDescription()}
-            </div>
-            <footer>
-              <ul className="Links">
-                {this.createLinks()}
-              </ul>
-            </footer>
-          </div>
+          </Link>
         </article>
       </li>
     )
@@ -43,25 +35,25 @@ class Project extends React.Component<InterfaceProjectComponent, {}> {
     e.target.parentNode.classList.toggle('active')
   }
 
-  private createDescription(descs = this.props.project.content) {
-    const descList: JSX.Element[] = []
+  // private createDescription(descs = this.props.project.content) {
+  //   const descList: JSX.Element[] = []
 
-    descs.map(desc => {
-      descList.push(<p key={desc}>{desc}</p>)
-    })
+  //   descs.map(desc => {
+  //     descList.push(<p key={desc}>{desc}</p>)
+  //   })
 
-    return descList
-  }
+  //   return descList
+  // }
 
-  private createLinks(links = this.props.project.links) {
-    const linkList: JSX.Element[] = []
+  // private createLinks(links = this.props.project.links) {
+  //   const linkList: JSX.Element[] = []
 
-    links.map(link => {
-      linkList.push(<li key={link.name}><a href={link.target}>{link.name}</a></li>)
-    })
+  //   links.map(link => {
+  //     linkList.push(<li key={link.name}><a href={link.target}>{link.name}</a></li>)
+  //   })
 
-    return linkList
-  }
+  //   return linkList
+  // }
 }
 
 export default Project

0616d81517557f2a5bc2470b3811df43cfd235dd
ctrlaltdev <yorick@ctrlaltdev.xyz>
2018-09-22 20:46:41 -0700

    add about me content

--- src/About/About.tsx
+++ src/About/About.tsx
@@ -1,11 +1,31 @@
 import * as React from 'react'
 import './About.css'
+import TechList from './TechList'
 
 class About extends React.Component {
+  public state: {
+    techs: JSX.Element[]
+  }
+  constructor(props: any) {
+    super(props)
+
+    this.state = {
+      techs: []
+    }
+  }
+
+  public componentWillMount() {
+    this.setState({techs: TechList})
+  }
+
   public render() {
     return(
-      <main>
-        me
+      <main className="About">
+        <h2>Yorick Demichelis</h2>
+        <p className="intro">I'm a tech enthusiast, front and back end developer, occasional tinkerer and infosec amateur.</p>
+        <ul className="techs">
+          {this.state.techs}
+        </ul>
       </main>
     )
   }

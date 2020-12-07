67af0dc813bf0002213da3a93744210e737d8bb3
Yorick <yorick@ctrlalt.dev>
2020-12-06 20:36:39 -0800

    feat: update fonts and colors

--- src/components/ProjectsList/index.js
+++ src/components/ProjectsList/index.js
@@ -3,13 +3,42 @@ import projects from '../../../static/projects.json'
 
 import './projectslist.sass'
 
-const getOffset = () => Math.round(Math.random() * 10000 + new Date().getTime())
+const colorScale = int => {
+  let r, g, b = 0
 
-const colors = ['Turquoise', 'Aquamarine', 'SteelBlue', 'Chartreuse', 'SpringGreen', 'ForestGreen', 'OliveDrab', 'Gold', 'Khaki', 'Coral', 'OrangeRed', 'DarkOrange', 'IndianRed', 'Salmon', 'FireBrick', 'Crimson', 'MediumVioletRed', 'HotPink', 'DeepPink', 'Orchid']
+  if (int >= 0 && int <= 20) {
+    r = 255
+    g = Math.round(12.75 * int)
+    b = 0
+  }
+  else if (int > 20 && int <= 40) {
+    r = Math.round(-12.75 * int + 510)
+    g = 255
+    b = 0
+  }
+  else if (int > 40 && int <= 60) {
+    r = 0
+    g = 255
+    b = Math.round(12.75 * int) - 510
+  }
+  else if (int > 60 && int <= 80) {
+    r = 0
+    g = Math.round(-12.75 * int + 1020)
+    b = 255
+  }
+  else {
+    r = Math.round(12.75 * int - 1020)
+    g = 0
+    b = 255
+  }
+
+  let h = r * 0x10000 + g * 0x100 + b * 0x1
+  return '#' + ('000000' + h.toString(16)).slice(-6)
+}
 
 export const ProjectItem = ({ project, color }) => {
   return (
-    <li className={ color }>
+    <li style={color}>
       <a href={ `/${project.id}/` }>{ project.name }</a>
     </li>
   )
@@ -20,9 +49,9 @@ const ProjectsList = () => {
     <ul className='projectslist'>
       {
         projects.map((p, i) => {
-          const offset = getOffset()
+          const color = { backgroundColor: colorScale(i * 100 / projects.length)  }
           return (
-            <ProjectItem key={ p.id } project={ p } color={ colors[(offset + i) % colors.length] } />
+            <ProjectItem key={ p.id } project={ p } color={ color } />
           )
         })
       }

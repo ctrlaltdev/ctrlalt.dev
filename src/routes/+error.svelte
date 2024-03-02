37dc38719f22efa0052b92ab880b86c07b8bf3f4
Yorick Demichelis <yorick@ctrlalt.dev>
2024-03-02 00:06:33 -0800

    feat: update the error page

--- src/routes/+error.svelte
+++ src/routes/+error.svelte
@@ -1,11 +1,12 @@
 <script lang="ts">
 	import { page } from '$app/stores'
+  import ShadowText from '$components/ShadowText.svelte'
 </script>
 
 <section class="Error">
-  <h1 class="Error__Title Layer Layer--cyan"><span>{$page.status}</span></h1>
-  <p class="Error__Title Layer Layer--magenta" aria-hidden><span>{$page.status}</span></p>
-  <p class="Error__Title Layer Layer--yellow" aria-hidden><span>{$page.status}</span></p>
+  <ShadowText let:shadowStyle>
+    <h1 class="Error__Title" style={shadowStyle}>{$page.status}</h1>
+  </ShadowText>
 </section>
 
 <style>
@@ -14,54 +15,14 @@
     position: relative;
   }
 
-  .Layer {
-    position: absolute;
-    top: 0;
-    bottom: 0;
-    left: 0;
-    right: 0;
-    mix-blend-mode: multiply;
-  }
-
-  @media (prefers-color-scheme: dark) {
-    .Layer {
-      mix-blend-mode: screen;
-    }
-  }
-
-  .Layer--cyan {
-    color: cyan;
-  }
-  .Layer--cyan span {
-    position: relative;
-    top: 0;
-    left: -1px;
-  }
-
-  .Layer--magenta {
-    color: magenta;
-  }
-  .Layer--magenta span {
-    position: relative;
-    top: 0;
-    left: 1px;
-  }
-
-  .Layer--yellow {
-    color: yellow;
-  }
-  .Layer--yellow span {
-    position: relative;
-    top: 2px;
-    left: 0;
-  }
-
   .Error__Title {
-    font-size: 20vw;
-    font-weight: 500;
     text-align: center;
     line-height: 75vh;
     letter-spacing: 1rem;
     font-family: var(--font-title);
+  }
+
+  .Error .Error__Title {
+    font-size: 20vh;
   }
 </style>
--- src/routes/404/+page.svelte
+++ src/routes/404/+page.svelte
@@ -1,7 +1,11 @@
+<script lang="ts">
+  import ShadowText from '$components/ShadowText.svelte'
+</script>
+
 <section class="Error">
-  <h1 class="Error__Title Layer Layer--cyan"><span>404</span></h1>
-  <p class="Error__Title Layer Layer--magenta" aria-hidden><span>404</span></p>
-  <p class="Error__Title Layer Layer--yellow" aria-hidden><span>404</span></p>
+  <ShadowText let:shadowStyle>
+    <h1 class="Error__Title" style={shadowStyle}>404</h1>
+  </ShadowText>
 </section>
 
 <style>
@@ -10,54 +14,14 @@
     position: relative;
   }
 
-  .Layer {
-    position: absolute;
-    top: 0;
-    bottom: 0;
-    left: 0;
-    right: 0;
-    mix-blend-mode: multiply;
-  }
-
-  @media (prefers-color-scheme: dark) {
-    .Layer {
-      mix-blend-mode: screen;
-    }
-  }
-
-  .Layer--cyan {
-    color: cyan;
-  }
-  .Layer--cyan span {
-    position: relative;
-    top: 0;
-    left: -1px;
-  }
-
-  .Layer--magenta {
-    color: magenta;
-  }
-  .Layer--magenta span {
-    position: relative;
-    top: 0;
-    left: 1px;
-  }
-
-  .Layer--yellow {
-    color: yellow;
-  }
-  .Layer--yellow span {
-    position: relative;
-    top: 2px;
-    left: 0;
-  }
-
   .Error__Title {
-    font-size: 20vw;
-    font-weight: 500;
     text-align: center;
     line-height: 75vh;
     letter-spacing: 1rem;
     font-family: var(--font-title);
+  }
+
+  .Error .Error__Title {
+    font-size: 20vh;
   }
 </style>
